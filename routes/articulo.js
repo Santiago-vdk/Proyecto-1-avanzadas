414419370e7d2391882b130e660152ff6e74a9c2
Rafael Angel Chacon Fallas <Rafael Angel Chacon Fallas>
2017-09-10 00:17:58 -0600

    actualizacion de consultas

--- routes/articulo.js
+++ routes/articulo.js
@@ -59,23 +59,21 @@ module.exports.set = function(app) {
 
 app.post('/api/v1/articulo', function(req, res) {
 
-  var name = req.body.nombre;
-  console.log(name);
-  // res.status(201).json({
-  //   status: "success",
-  //   data: name
-  // });
-
   var destino = req.query.origin || "heredia";
 
   // Default siempre desde heredia
   const columns = ['id', 'id_tipo', 'nombre', 'precio'];
-  databaseConfig.getDb(destino).query('SELECT ${columns^} FROM articulo', {
+  var id = req.body.id;
+  var id_tipo = req.body.id_tipo;
+  var nombre = req.body.nombre;
+  var precio = req.body.precio;
+  var myquery = 'INSERT INTO public.articulo(${columns^}) VALUES ('+id+','+ id_tipo+','+ nombre+','+  precio+')';
+
+  databaseConfig.getDb(destino).query(myquery, {
       columns: columns.map(pgp.as.name).join(),
       table: 'Table Name'
     }).then(result => {
       console.log(result); // printing the data returned
-
       res.status(200).json({
         status: "success",
         data: result
@@ -87,7 +85,7 @@ app.post('/api/v1/articulo', function(req, res) {
         if (destino.localeCompare('heredia') == 0) {
           console.log("El nodo central no se encuentra disponible, insertando en SanJose");
 
-          databaseConfig.getDb('sanjose').query('SELECT ${columns^} FROM articulo', {
+          databaseConfig.getDb('sanjose').query(myquery, {
               columns: columns.map(pgp.as.name).join(),
               table: 'Table Name'
             }).then(result => {
@@ -109,7 +107,7 @@ app.post('/api/v1/articulo', function(req, res) {
         } else {
           if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
             console.log('Error de conexion, realizando consulta en nodo principal Heredia');
-            databaseConfig.getDb('heredia').query('SELECT ${columns^} FROM articulo', {
+            databaseConfig.getDb('heredia').query(myquery, {
                 columns: columns.map(pgp.as.name).join(),
                 table: 'Table Name'
               }).then(result => {
@@ -137,51 +135,203 @@ app.post('/api/v1/articulo', function(req, res) {
 })
 
 app.put('/api/v1/articulo', function(req, res) {
-  var name = req.body.nombre;
-  console.log(name);
-  res.status(201).json({
-    status: "success",
-    data: name
-  });
+  var destino = req.query.origin || "heredia";
+  // Default siempre desde heredia
+  var id = req.body.id;
+  var nombre = req.body.nombre;
+  var precio = req.body.precio;
+
+  var myquery = 'UPDATE articulo SET nombre = '+nombre+' precio = '+precio+' WHERE id = '+id;
+
+  databaseConfig.getDb(destino).query(myquery, {
+      columns: columns.map(pgp.as.name).join(),
+      table: 'Table Name'
+    }).then(result => {
+      console.log(result); // printing the data returned
+      res.status(200).json({
+        status: "success",
+        data: result
+      });
+
+    })
+    .catch(error => {
+        var error_type = error.code.substring(0, 2);
+        if (destino.localeCompare('heredia') == 0) {
+          console.log("El nodo central no se encuentra disponible, insertando en SanJose");
+
+          databaseConfig.getDb('sanjose').query(myquery, {
+              columns: columns.map(pgp.as.name).join(),
+              table: 'Table Name'
+            }).then(result => {
+              console.log(result); // printing the data returned
+
+              res.status(200).json({
+                status: "success",
+                data: result
+              });
+
+            })
+            .catch(error => {
+              console.log("Nodo secundario SJ fuera de linea..."); // printing the error
+              res.status(500).send();
+            });
+
+
+
+        } else {
+          if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
+            console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+            databaseConfig.getDb('heredia').query(myquery, {
+                columns: columns.map(pgp.as.name).join(),
+                table: 'Table Name'
+              }).then(result => {
+                console.log(result); // printing the data returned
+
+                res.status(200).json({
+                  status: "success",
+                  data: result
+                });
+
+              })
+              .catch(error => {
+                console.log("Nodo central fuera de linea..."); // printing the error
+                res.status(500).send();
+              });
+          } else {
+            console.log("Error inesperado"); // printing the error
+            res.status(500).send();
+          }
+        }
+      })
 })
 
 app.delete('/api/v1/articulo', function(req, res) {
-  var id = req.body.id;
-  console.log(id);
-  res.status(200).json({
-    status: "success",
-    data: id
-  });
-})
-
-
-app.param('id', function(req, res, next, id) {
-  req.id = id;
-  next();
-});
-
-
-
-app.get('/api/v1/articulo/:pk?', function(req, res) {
-
   var destino = req.query.origin || "heredia";
-
   // Default siempre desde heredia
-  const columns = ['id', 'id_tipo', 'nombre', 'precio'];
-  databaseConfig.getDb(destino).query('SELECT ${columns^} FROM articulo WHERE id=pk', {
+  var id = req.body.id;
+
+  var myquery = 'UPDATE articulo SET activo = false WHERE id = '+id;
+
+  databaseConfig.getDb(destino).query(myquery, {
       columns: columns.map(pgp.as.name).join(),
       table: 'Table Name'
-    }).then(data => {
-      console.log(data); // printing the data returned
+    }).then(result => {
+      console.log(result); // printing the data returned
       res.status(200).json({
         status: "success",
-        data: data
+        data: result
       });
     })
     .catch(error => {
-      console.log(error); // printing the error
-      res.status(500).send();
-    });
+        var error_type = error.code.substring(0, 2);
+        if (destino.localeCompare('heredia') == 0) {
+          console.log("El nodo central no se encuentra disponible, insertando en SanJose");
 
-});
+          databaseConfig.getDb('sanjose').query(myquery, {
+              columns: columns.map(pgp.as.name).join(),
+              table: 'Table Name'
+            }).then(result => {
+              console.log(result); // printing the data returned
+              res.status(200).json({
+                status: "success",
+                data: result
+              });
+            })
+            .catch(error => {
+              console.log("Nodo secundario SJ fuera de linea..."); // printing the error
+              res.status(500).send();
+            });
+
+        } else {
+          if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
+            console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+            databaseConfig.getDb('heredia').query(myquery, {
+                columns: columns.map(pgp.as.name).join(),
+                table: 'Table Name'
+              }).then(result => {
+                console.log(result); // printing the data returned
+
+                res.status(200).json({
+                  status: "success",
+                  data: result
+                });
+
+              })
+              .catch(error => {
+                console.log("Nodo central fuera de linea..."); // printing the error
+                res.status(500).send();
+              });
+          } else {
+            console.log("Error inesperado"); // printing the error
+            res.status(500).send();
+          }
+        }
+      })
+})
+
+
+
+app.get('/api/v1/articulo/:id?', function(req, res) {
+
+  var destino = req.query.origin || "heredia";
+  // Default siempre desde heredia
+
+  var myquery = 'SELECT ${columns^} FROM articulo WHERE id = '+req.params.id;
+
+  databaseConfig.getDb(destino).query(myquery, {
+      columns: columns.map(pgp.as.name).join(),
+      table: 'Table Name'
+    }).then(result => {
+      console.log(result); // printing the data returned
+      res.status(200).json({
+        status: "success",
+        data: result
+      });
+    })
+    .catch(error => {
+        var error_type = error.code.substring(0, 2);
+        if (destino.localeCompare('heredia') == 0) {
+          console.log("El nodo central no se encuentra disponible, insertando en SanJose");
+
+          databaseConfig.getDb('sanjose').query(myquery, {
+              columns: columns.map(pgp.as.name).join(),
+              table: 'Table Name'
+            }).then(result => {
+              console.log(result); // printing the data returned
+              res.status(200).json({
+                status: "success",
+                data: result
+              });
+            })
+            .catch(error => {
+              console.log("Nodo secundario SJ fuera de linea..."); // printing the error
+              res.status(500).send();
+            });
+
+        } else {
+          if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
+            console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+            databaseConfig.getDb('heredia').query(myquery, {
+                columns: columns.map(pgp.as.name).join(),
+                table: 'Table Name'
+              }).then(result => {
+                console.log(result); // printing the data returned
+
+                res.status(200).json({
+                  status: "success",
+                  data: result
+                });
+
+              })
+              .catch(error => {
+                console.log("Nodo central fuera de linea..."); // printing the error
+                res.status(500).send();
+              });
+          } else {
+            console.log("Error inesperado"); // printing the error
+            res.status(500).send();
+          }
+        }
+      })
+    });
 };
