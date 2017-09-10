d415ab15cb9d22b4ea423596f39a4a8db10a7b0c
Rafael Angel Chacon Fallas <Rafael Angel Chacon Fallas>
2017-09-10 03:11:51 -0600

    modificaciones en cliente, empleado y tienda

--- routes/empleado.js
+++ routes/empleado.js
@@ -6,6 +6,58 @@ module.exports.set = function(app) {
 
   app.get('/api/v1/empleado', function(req, res) {
     var destino = req.query.origin || "heredia";
+    const columns = ['id', 'id_tienda', 'id_puesto','nombre'];
+    var myquery = 'SELECT (${columns^}) FROM empleado WHERE activo = true';
+
+      databaseConfig.getDb(destino).query(myquery, {
+
+          table: 'Table Name'
+        }).then(result => {
+          console.log(result); // printing the data returned
+
+          res.status(200).json({
+            status: "success",
+            data: result
+          });
+
+        })
+        .catch(error => {
+            var error_type = error.code.substring(0, 2);
+            if (destino.localeCompare('heredia') == 0) {
+              console.log("Nodo central fuera de linea..."); // printing the error
+              res.status(500).send();
+            } else {
+              if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
+                console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+                databaseConfig.getDb('heredia').query(myquery, {
+
+                    table: 'Table Name'
+                  }).then(result => {
+                    console.log(result); // printing the data returned
+
+                    res.status(200).json({
+                      status: "success",
+                      data: result
+                    });
+
+                  })
+                  .catch(error => {
+                    console.log("Nodo central fuera de linea..."); // printing the error
+                    res.status(500).send();
+                  });
+              } else {
+                console.log("Error inesperado"); // printing the error
+                res.status(500).send();
+              }
+            }
+
+          })
+  });
+
+
+
+  app.get('/api/v1/empleado_v', function(req, res) {
+    var destino = req.query.origin || "heredia";
     var myquery = 'SELECT * FROM empleado_v';
 
       databaseConfig.getDb(destino).query(myquery, {
--- routes/tienda.js
+++ routes/tienda.js
@@ -6,6 +6,58 @@ module.exports.set = function(app) {
 
   app.get('/api/v1/tienda', function(req, res) {
     var destino = req.query.origin || "heredia";
+    const columns = ['id', 'id_sucursal', 'nombre'];
+    var myquery = 'SELECT (${columns^}) FROM tienda WHERE activo = true';
+
+          databaseConfig.getDb(destino).query(myquery, {
+
+              table: 'Table Name'
+            }).then(result => {
+              console.log(result); // printing the data returned
+
+              res.status(200).json({
+                status: "success",
+                data: result
+              });
+
+            })
+            .catch(error => {
+                var error_type = error.code.substring(0, 2);
+                if (destino.localeCompare('heredia') == 0) {
+                  console.log("Nodo central fuera de linea..."); // printing the error
+                  res.status(500).send();
+                } else {
+                  if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
+                    console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+                    databaseConfig.getDb('heredia').query(myquery, {
+
+                        table: 'Table Name'
+                      }).then(result => {
+                        console.log(result); // printing the data returned
+
+                        res.status(200).json({
+                          status: "success",
+                          data: result
+                        });
+
+                      })
+                      .catch(error => {
+                        console.log("Nodo central fuera de linea..."); // printing the error
+                        res.status(500).send();
+                      });
+                  } else {
+                    console.log("Error inesperado"); // printing the error
+                    res.status(500).send();
+                  }
+                }
+
+              })
+  });
+
+
+
+  app.get('/api/v1/tienda_v', function(req, res) {
+    var destino = req.query.origin || "heredia";
     var myquery = 'SELECT * FROM tienda_v';
 
           databaseConfig.getDb(destino).query(myquery, {
@@ -55,7 +107,7 @@ module.exports.set = function(app) {
 
   app.get('/api/v1/tienda/:id?', function(req, res) {
     var destino = req.query.origin || "heredia";
-    var myquery = 'SELECT ${columns^} FROM tienda_v WHERE id = '+req.params.id;
+    var myquery = 'SELECT * FROM tienda_v WHERE id = '+req.params.id;
 
     databaseConfig.getDb(destino).query(myquery, {
 
