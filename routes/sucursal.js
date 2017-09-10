9fc0a0569aafca6b1882ddcd0c47d60b3ea33395
Rafael Angel Chacon Fallas <Rafael Angel Chacon Fallas>
2017-09-10 13:12:38 -0600

    cambios menores

--- routes/sucursal.js
+++ routes/sucursal.js
@@ -7,7 +7,7 @@ module.exports.set = function(app) {
   app.get('/api/v1/sucursal', function(req, res) {
     var destino = req.query.origin || "heredia";
       const columns = ['id','nombre'];
-      var myquery = 'SELECT ${columns^} FROM sucursal WHERE activo = true';
+      var myquery = 'SELECT (id,nombre) FROM sucursal WHERE activo = true';
 
         databaseConfig.getDb(destino).query(myquery, {
             columns: columns.map(pgp.as.name).join(),
@@ -104,6 +104,79 @@ module.exports.set = function(app) {
 
             })
   });
+
+
+  app.post('/api/v1/sucursal', function(req, res) {
+
+    var destino = req.query.origin || "heredia";
+    // Default siempre desde heredia
+    const columns = ['nombre'];
+    var nombre = req.body.nombre;
+
+    var myquery = 'INSERT INTO public.sucursal(nombre) VALUES (\"'+nombre+'\")';
+
+    databaseConfig.getDb(destino).query(myquery, {
+        table: 'Table Name'
+      }).then(result => {
+        console.log(result); // printing the data returned
+        res.status(200).json({
+          status: "success",
+          data: result
+        });
+
+      })
+      .catch(error => {
+          // var error_type = error.code.substring(0, 2);
+          // if (destino.localeCompare('heredia') == 0) {
+          //   console.log("El nodo central no se encuentra disponible, insertando en SanJose");
+          //
+          //   databaseConfig.getDb('sanjose').query(myquery, {
+          //       table: 'Table Name'
+          //     }).then(result => {
+          //       console.log(result); // printing the data returned
+          //
+          //       res.status(200).json({
+          //         status: "success",
+          //         data: result
+          //       });
+          //
+          //     })
+          //     .catch(error => {
+          //       console.log("Nodo secundario SJ fuera de linea..."); // printing the error
+          //       res.status(500).send();
+          //     });
+          //
+          //
+          //
+          // } else {
+          //   if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
+          //     console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+          //     databaseConfig.getDb('heredia').query(myquery, {
+          //         table: 'Table Name'
+          //       }).then(result => {
+          //         console.log(result); // printing the data returned
+          //
+          //         res.status(200).json({
+          //           status: "success",
+          //           data: result
+          //         });
+          //
+          //       })
+          //       .catch(error => {
+          //         console.log("Nodo central fuera de linea..."); // printing the error
+          //         res.status(500).send();
+          //       });
+          //   } else {
+          //     console.log("Error inesperado"); // printing the error
+          //     res.status(500).send();
+          //   }
+          // }
+
+        })
+
+
+  })
+
 
   app.get('/api/v1/sucursal/:id?', function(req, res) {
     var destino = req.query.origin || "heredia";
