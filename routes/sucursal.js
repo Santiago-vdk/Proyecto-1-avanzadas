8bdf3879f2ed1458627f008b1166cb3113706c88
Santiago VdeK <santiago.vdk@gmail.com>
2017-09-11 00:41:20 -0600

    recover

--- routes/sucursal.js
+++ routes/sucursal.js
@@ -2,7 +2,7 @@ module.exports.set = function(app) {
   var direccionador = require('../logic/direccionador');
   var databaseConfig = require('../configs/database');
   var pgp = databaseConfig.getPgp();
-  var debug = false;
+  var debug = true;
 
   app.get('/api/v1/sucursal', function(req, res) {
     var destino = req.query.origin || 1;
@@ -26,36 +26,35 @@ module.exports.set = function(app) {
       })
       .catch(error => {
         if (debug) {
-          console.log(error); // printing the data returned
+          console.log(error.code); // printing the data returned
         }
-        // var error_type = error.code.substring(0, 2);
-        // if (destino.localeCompare('heredia') == 0) {
-        //   console.log("Nodo central fuera de linea..."); // printing the error
-        //   res.status(500).send();
-        // } else {
-        //   if ((error_type.localeCompare('08') == 0) || (error_type.localeCompare('28') == 0)) {
-        //     console.log('Error de conexion, realizando consulta en nodo principal Heredia');
-        //     databaseConfig.getDb('heredia').query(myquery, {
-        //
-        //         table: 'Table Name'
-        //       }).then(result => {
-        //         console.log(result); // printing the data returned
-        //
-        //         res.status(200).json({
-        //           status: "success",
-        //           data: result
-        //         });
-        //
-        //       })
-        //       .catch(error => {
-        //         console.log("Nodo central fuera de linea..."); // printing the error
-        //         res.status(500).send();
-        //       });
-        //   } else {
-        //     console.log("Error inesperado"); // printing the error
-        //     res.status(500).send();
-        //   }
-        // }
+        if (destino === 1) {
+          console.log("Nodo principal fuera de linea..."); // printing the error
+          res.status(500).send();
+        } else {
+          if (error.code.localeCompare("ETIMEDOUT") === 0) {
+            console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+            databaseConfig.getDb(1).query(myquery, {
+                columns: columns.map(pgp.as.name).join(),
+                table: 'Table Name'
+              }).then(result => {
+                console.log(result); // printing the data returned
+
+                res.status(200).json({
+                  status: "success",
+                  data: result
+                });
+
+              })
+              .catch(error => {
+                console.log("Nodo central fuera de linea..."); // printing the error
+                res.status(500).send();
+              });
+          } else {
+            console.log("Error inesperado"); // printing the error
+            res.status(500).send();
+          }
+        }
 
       })
   });
