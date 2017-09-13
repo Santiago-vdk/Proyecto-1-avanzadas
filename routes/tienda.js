6eada8102fe2df65bd6a4cb80fff91b2f7ce8286
Rafael Angel Chacon Fallas <Rafael Angel Chacon Fallas>
2017-09-13 17:34:35 -0600

    consulta tiendas totales

--- routes/tienda.js
+++ routes/tienda.js
@@ -58,6 +58,60 @@ module.exports.set = function(app) {
   });
 
 
+  app.get('/api/v1/tienda/total', function(req, res) {
+    var destino = req.query.origin || 1;
+    const columns = ['id', 'id_sucursal', 'nombre'];
+    var myquery = 'SELECT ${columns^} FROM tienda WHERE activo = true';
+
+    databaseConfig.getDb(destino).query(myquery, {
+        columns: columns.map(pgp.as.name).join(),
+        table: 'Table Name'
+      }).then(result => {
+        console.log("Realizando get tiendas");
+        if (debug) {
+          console.log(result); // printing the data returned
+        }
+        res.status(200).json({
+          status: "success",
+          data: result
+        });
+
+      })
+      .catch(error => {
+        if (debug) {
+          console.log(error); // printing the data returned
+        }
+
+
+        if (destino === 1) {
+          console.log("Nodo central fuera de linea..."); // printing the error
+          res.status(500).send();
+        } else {
+          console.log('Error de conexion, realizando consulta en nodo principal Heredia');
+          var myquery = 'SELECT ${columns^} FROM tienda WHERE activo = true';
+          databaseConfig.getDb(1).query(myquery, {
+              columns: columns.map(pgp.as.name).join(),
+              table: 'Table Name'
+            }).then(result => {
+              console.log(result); // printing the data returned
+
+              res.status(200).json({
+                status: "success",
+                data: result
+              });
+
+            })
+            .catch(error => {
+              console.log("Nodo central fuera de linea..."); // printing the error
+              res.status(500).send();
+            });
+
+        }
+      })
+
+  });
+
+
 
   app.get('/api/v1/tienda_v', function(req, res) {
     var destino = req.query.origin || 1;
